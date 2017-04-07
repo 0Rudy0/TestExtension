b53d8066fe586d0672415b357a611741980cfa74
danijel_rudman <rudman0@gmail.com>
2017-04-07 15:38:32 +0200

    github complete, facebook started

--- modules/facebook.js
+++ modules/facebook.js
@@ -5,9 +5,10 @@
 	Adopto.contentScript = {
 		name: 'Facebook',
 		sourceType: 1,
+		callback: null,
 
 		isProfilePageActive: function () {
-			if ($('#fbCoverImageContainer').length) {
+			if ($('img.coverPhotoImg.photo.img').length) {
 				return true;
 			}
 
@@ -20,6 +21,58 @@
 			}
 
 			return false;
+		},
+
+		getData: function (callback) {
+			Adopto.contentScript.callback = callback;
+			var m = candidateDataModel;
+			m.mainData.fullName = $('#fb-timeline-cover-name') ? $('#fb-timeline-cover-name').html().trim() : '';
+			//m.mainData.title = $('[itemprop=worksFor]').attr('title') ? $('[itemprop=worksFor]').attr('title').trim() : '';
+			var intro = $('#intro_container_id');
+			m.mainData.summary = intro.find('#profile_intro_card_bio')[0].innerText;
+			var lis = intro.find('li');
+
+			for (var i = 0; i < lis.length; i++) {
+				if ($(lis[i]).find('i.img.sx_3adcbb').length > 0) {
+					m.mainData.title = $(lis[i]).find('div._50f3')[0].innerText;
+				}
+				else if ($(lis[i]).find('i.img.sx_a97b39').length > 0) {
+					m.mainData.location = $(lis[i]).find('div._50f3')[0].innerText;
+				}
+			}
+			//m.mainData.contactInfo.email = $('li[itemprop="email"] a').html() ? $('li[itemprop="email"] a').html().trim() : '';
+			//m.mainData.location = $('[itemprop=homeLocation]') && $('[itemprop=homeLocation]').attr('title') ? $('[itemprop=homeLocation]').attr('title').trim() : '';
+			//m.mainData.profileImgUrl = $('.vcard-avatar img.avatar') ? $('.vcard-avatar img.avatar').attr('src').trim() : '';
+			//m.mainData.socialNetworks.website = $('.vcard-details li.vcard-detail[aria-label="Blog or website"] a').html() ? $('.vcard-details li.vcard-detail[aria-label="Blog or website"] a').html().trim() : '';
+			//m.mainData.summary = $('.user-profile-bio').length > 0 ? $('.user-profile-bio')[0].innerText : '';
+
+
+			//var username = $('span.vcard-username').html();
+			//Adopto.contentScript.getSkills(username, m);
+			//m.mainData.skills = Adopto.contentScript.getSkills(m);
+
+			callback(m);
+		},
+
+		getSkills: function (username, m) {
+			//$.ajax({
+			//	url: 'https://github.com/' + username + '?tab=repositories',
+			//	async: true,
+			//	cache: true,
+			//	success: function (response) {
+			//		var langs = $(response).find('span[itemprop="programmingLanguage"]');
+			//		for (var i = 0; i < langs.length; i++) {
+			//			if (m.mainData.skills.indexOf(langs[i].innerText.trim()) < 0) {
+			//				m.mainData.skills.push(langs[i].innerText.trim());
+			//			}
+			//		}
+			//		Adopto.contentScript.callback(m);
+			//	},
+			//	error: function (error) {
+			//		console.log(error);
+			//	}
+			//})
+			
 		},
 
 		getProfilePageUrl: function () {
--- modules/github.js
+++ modules/github.js
@@ -15,23 +15,20 @@
 			return false;
 		},
 
-		//isProfileMoreInfoActive: function () {
-		//	return false;
-		//},
-
 		getData: function (callback) {
 			Adopto.contentScript.callback = callback;
 			var m = candidateDataModel;
-			m.mainData.fullName = $('.vcard-fullname').text();
-			m.mainData.title = $('[itemprop=worksFor]').attr('title');
-			m.mainData.contactInfo.email = $('li[itemprop="email"] a').html();
-			m.mainData.location = $('[itemprop=homeLocation]').attr('title');
-			m.mainData.profileImgUrl = $('.vcard-avatar img.avatar').attr('src');
-			m.mainData.socialNetworks.website = $('.vcard-details li.vcard-detail[aria-label="Blog or website"] a').html();
+			m.mainData.fullName = $('.vcard-fullname') ? $('.vcard-fullname').text().trim() : '';
+			m.mainData.title = $('[itemprop=worksFor]').attr('title') ? $('[itemprop=worksFor]').attr('title').trim() : '';
+			m.mainData.contactInfo.email = $('li[itemprop="email"] a').html() ? $('li[itemprop="email"] a').html().trim() : '';
+			m.mainData.location = $('[itemprop=homeLocation]') && $('[itemprop=homeLocation]').attr('title') ? $('[itemprop=homeLocation]').attr('title').trim() : '';
+			m.mainData.profileImgUrl = $('.vcard-avatar img.avatar') ? $('.vcard-avatar img.avatar').attr('src').trim() : '';
+			m.mainData.socialNetworks.website = $('.vcard-details li.vcard-detail[aria-label="Blog or website"] a').html() ? $('.vcard-details li.vcard-detail[aria-label="Blog or website"] a').html().trim() : '';
 			m.mainData.summary = $('.user-profile-bio').length > 0 ? $('.user-profile-bio')[0].innerText : '';
 
 
 			var username = $('span.vcard-username').html();
+			m.mainData.socialNetworks.github = 'https://github.com/' + username;
 			Adopto.contentScript.getSkills(username, m);
 			//m.mainData.skills = Adopto.contentScript.getSkills(m);
 
@@ -40,7 +37,7 @@
 
 		getSkills: function (username, m) {
 			$.ajax({
-				url: 'https://github.com/' + username + '?tab=repositories',
+				url: m.mainData.socialNetworks.github + '?tab=repositories',
 				async: true,
 				cache: true,
 				success: function (response) {
@@ -58,33 +55,6 @@
 			})
 			
 		}
-
-		//getProfilePageUrl: function () {
-		//	return window.location.href;
-		//},
-
-		//getName: function () {
-		//	return $('.vcard-fullname').text();
-		//},
-
-		//getEmail: function () {
-		//	return '';
-		//},
-
-		//getJobTitle: function () {
-		//	return $('[itemprop=worksFor]').attr('title');
-		//},
-
-		//getLocation: function () {
-		//	return $('[itemprop=homeLocation]').attr('title');
-		//},
-
-		//getProfileImageURL: function () {
-		//	return $('[itemprop=image] img').attr('src');
-		//},
-
-		//buttonPlaceholder: function (button) {
-		//}
 	};
 
 })(jQuery, Adopto.hostTest);
