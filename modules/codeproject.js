432f8d31f76e7d2efa732bf3d28181af5a9ed81b
danijel_rudman <rudman0@gmail.com>
2017-04-14 11:19:16 +0200

    Codeproject, stackoverflow

--- modules/codeproject.js
+++ modules/codeproject.js
@@ -1,5 +1,10 @@
 (function ($, host) {
 
+	//example profiles
+	// https://www.codeproject.com/script/Membership/View.aspx?mid=40863
+	// https://www.codeproject.com/script/Membership/View.aspx?mid=4425742
+	// https://www.codeproject.com/script/Membership/View.aspx?mid=36803
+
 	if (!host('codeproject.com', 'www.codeproject.com')) return;
 
 	Adopto.contentScript = {
@@ -28,12 +33,42 @@
 			cd.mainData.location = $('#ctl00_MC_Prof_MemberImage').parent().text().trim();
 			cd.mainData.profileImgUrl = $('#ctl00_MC_Prof_MemberImage').attr('src');
 			cd.mainData.title = $('#ctl00_MC_Prof_MemberProRow').text().trim();
-			//cd.mainData.summary = $('.bio p').html();
-
+			cd.mainData.summary = $('.member-profile>tbody>tr>td>div:nth-child(3)').html();
+			if (cd.mainData.summary.indexOf('<br') > -1) {
+				cd.mainData.summary = cd.mainData.summary.substring(0, cd.mainData.summary.indexOf('<br'));
+			}
 			cd.mainData.socialNetworks.codeproject = 'http://codeproject.com' + $('.username a').attr('href');
-			//cd.mainData.socialNetworks.twitter = $('.icon-twitter').parent().find('a').attr('href');
 
-			Adopto.contentScript.callback(cd);
+			var links = $('.member-rep-list.extended>tbody>tr>td:nth-child(2) a');
+			for (var i = 0; i < links.length; i++) {
+				if (links[i].getAttribute('href').indexOf('twitter.com') > -1) {
+					cd.mainData.socialNetworks.twitter = links[i].getAttribute('href');
+				}
+				else if (links[i].getAttribute('href').indexOf('linkedin.com') > -1) {
+					cd.mainData.socialNetworks.linkedin = links[i].getAttribute('href');
+				}
+				else if (links[i].getAttribute('title').indexOf('View member\'s homepage') > -1) {
+					cd.mainData.socialNetworks.website = links[i].getAttribute('title');
+				}
+			}
+
+			var articlesLink = $('#ctl00_MC_Prof_ArticleContributionLink').attr('href');
+			$.ajax({
+				url: articlesLink,
+				async: true,
+				cache: true,
+				success: function (data) {
+					var skills = $(data).find('table.padded>tbody>tr>td>h5');
+					for (var i = 0; i < skills.length; i++) {
+						if (candidateDataModel.mainData.skills.indexOf(skills[i].innerText)) {
+							candidateDataModel.mainData.skills.push(skills[i].innerText);
+						}
+
+					}
+					Adopto.contentScript.callback(cd);
+				}
+			});
+
 		}
 	};
 
